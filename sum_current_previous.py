527b38f562e277abc64508b3b34bf4b28d632742
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-20 20:57:27 +0800

    pseudocode

--- sum_current_previous.py
+++ sum_current_previous.py
@@ -1,3 +1,13 @@
 # Exercise 2: Print the sum of the current number and the previous number
 # Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
 
+# Pseudocode 
+
+# Creating a variable to start the loop from 0 
+# Creating loop 
+# Showing the result 
+
+   
+
+
+    
